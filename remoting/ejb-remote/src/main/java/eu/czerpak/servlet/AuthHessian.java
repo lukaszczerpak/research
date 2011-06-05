8181d2c23cf6547c23cb03a7f49b905d272adce0
Łukasz Czerpak <lukasz.czerpak@softnet.com.pl>
2011-06-05 19:25:44 +0200

    added some new session creation info

--- remoting/ejb-remote/src/main/java/eu/czerpak/servlet/AuthHessian.java
+++ remoting/ejb-remote/src/main/java/eu/czerpak/servlet/AuthHessian.java
@@ -4,7 +4,10 @@ import com.caucho.hessian.server.HessianServlet;
 import eu.czerpak.bean.AuthBean;
 import eu.czerpak.service.Authorization;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,6 +20,14 @@ public class AuthHessian extends HessianServlet implements Authorization
 {
     @Inject
     AuthBean authBean;
+
+    private static AtomicInteger instanceCount = new AtomicInteger(0);
+
+    @PostConstruct
+    public void onNewSession(){
+        System.out.println("On new session(" + instanceCount + "): " + new Date());
+        instanceCount.incrementAndGet();
+    }
 
     @Override
     public void auth(String login, String password)
